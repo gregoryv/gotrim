b3f5f86085a133aaa2f3e27ced5ce90639bada16
Gregory Vincic <gregoryvse@gmail.com>
2022-12-21 06:26:01 +0100

    Use trim.Trimmer

--- cmd/trim/main.go
+++ cmd/trim/main.go
@@ -1,41 +1,24 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"os"
-	"strings"
 
 	"github.com/gregoryv/cmdline"
-	"github.com/gregoryv/vt100"
+	"github.com/gregoryv/trim"
 )
 
 func main() {
 	var (
-		cli    = cmdline.NewBasicParser()
-		cols   = cli.Option("-c, --columns").Int(72)
-		suffix = cli.Option("-s, --suffix").String("...")
-
-		tabWidth = cli.Option("-t, --tab-width", "number of spaces").Int(4)
-		tab      = strings.Repeat(" ", tabWidth)
+		cli = cmdline.NewBasicParser()
+		t   = trim.Trimmer{
+			Columns:     cli.Option("-c, --columns").Int(72),
+			Suffix:      cli.Option("-s, --suffix").String("..."),
+			TabWidth:    cli.Option("-t, --tab-width", "number of spaces").Int(4),
+			Home:        os.Getenv("HOME"),
+			ReplaceHome: true,
+		}
 	)
 	cli.Parse()
-	s := bufio.NewScanner(os.Stdin)
 
-	home := os.Getenv("HOME")
-	at := vt100.Attributes()
-	for s.Scan() {
-		line := s.Text()
-		line = strings.ReplaceAll(line, home, "~")
-		// replace tabs
-		if tabWidth > 0 {
-			line = strings.ReplaceAll(line, "\t", tab)
-		}
-
-		if len(line) > cols {
-			line = fmt.Sprintf("%s%s%v", line[:cols], at.Reset, suffix)
-		}
-
-		fmt.Println(line)
-	}
+	t.Trim(os.Stdout, os.Stdin)
 }
